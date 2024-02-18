9ad5a3e4581682828eb0247edf293de1f6f16690
Augusto Kussema <dev.kussema@gmail.com>
2024-02-18 16:14:52 +0100

    O que mais falta fazer

--- app/Prada/Controllers/EstoqueController.php
+++ app/Prada/Controllers/EstoqueController.php
@@ -4,7 +4,11 @@ namespace App\Prada\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\{AreaHospitalar as AH, Estoque, SaldoEstoque as SE, ProdutoEstoque as PE};
+use App\Models\{
+    AreaHospitalar as AH, Estoque,
+    SaldoEstoque as SE, ProdutoEstoque as PE,
+    Atividade
+};
 
 class EstoqueController extends Controller
 {
@@ -75,6 +79,11 @@ class EstoqueController extends Controller
             'area_hospitalar_id' => auth()->user()->area_hospitalar->area_hospitalar_id
         ]);
 
+        Atividade::create([
+            'texto' => "Adicionou cerca de {$request->qtd} {$request->forma} de {$request->designacao} para ".auth()->user()->area_hospitalar->area_hospitalar->nome,
+            'user_id' => auth()->user()->id
+        ]);
+
         return response()->json(['message' => "{$request->designacao} adicionado!"]);
     }
 
@@ -113,7 +122,7 @@ class EstoqueController extends Controller
             'produto_estoque_id' => $novoProduto->id,
             'qtd' => $qtdBaixar
         ]);
-        
+
         $estoque = Estoque::create([
             'produto_estoque_id' => $novoProduto->id,
             'area_hospitalar_id' => $request->area_hospitalar_id
--- resources/views/default/home/index.blade.php
+++ resources/views/default/home/index.blade.php
@@ -75,8 +75,8 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <ul class="list-inline mb-0">
-                                <li class="list-inline-item"><a href="privacy-policy.html">Privacy Policy</a></li>
-                                <li class="list-inline-item"><a href="terms-of-service.html">Terms of Use</a></li>
+                                <li class="list-inline-item"><a href="#">Politicas de Privacidade</a></li>
+                                <li class="list-inline-item"><a href="#">Termos de Uso</a></li>
                             </ul>
                         </div>
                         <div class="col-lg-6 text-right">
@@ -84,7 +84,7 @@
                                 <script>
                                     document.write(new Date().getFullYear())
                                 </script>©
-                            </span> <a href="#" class="">POS Dash</a>.
+                            </span> <a href="#" class="">{{ env('APP_NAME') }}</a>.
                         </div>
                     </div>
                 </div>
