249bd7d5228b8357f038a68a2e461f9547e4047a
Augusto Kussema <dev.kussema@gmail.com>
2024-02-27 22:51:30 +0100

    Config do email

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => (nem('PC_HOST') !== null) ? nem('mail.mailer') : env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -37,11 +37,11 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => (nem('PC_HOST') !== null) ? nem('mail.host') : env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => (nem('PC_HOST') !== null) ? nem('mail.port') : env('MAIL_PORT', 587),
+            'encryption' => (nem('PC_HOST') !== null) ? nem('mail.encryption') : env('MAIL_ENCRYPTION', 'tls'),
+            'username' => (nem('PC_HOST') !== null) ? nem('mail.username') : env('MAIL_USERNAME'),
+            'password' => (nem('PC_HOST') !== null) ? nem('mail.password') : env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
@@ -108,8 +108,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => (nem('PC_HOST') !== null) ? nem('mail.from_address') : env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => (nem('PC_HOST') !== null) ? nem('mail.from_name') : env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
