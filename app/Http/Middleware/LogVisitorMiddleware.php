598000d4c53fc064b81a6b0b19ad2dc8d4db931b
Augusto Kussema <dev.kussema@gmail.com>
2024-03-05 23:12:16 +0100

    Mais uma correção

--- app/Http/Middleware/LogVisitorMiddleware.php
+++ app/Http/Middleware/LogVisitorMiddleware.php
@@ -17,7 +17,6 @@ class LogVisitorMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        /*
         // Verificar se o dispositivo com este IP acessou o sistema hoje
         $visitor = Visitor::where('ip', $request->ip())
             ->where('device', $request->header('User-Agent'))
@@ -42,7 +41,7 @@ class LogVisitorMiddleware
                 'referrer' => $request->header('referer'), // referência do site
                 'device' => $request->header('User-Agent'),
             ]);
-        } */
+        }
         return $next($request);
     }
 
