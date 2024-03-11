6677f9b44ce438a0cd65cdbcd7883dca218c18fc
Augusto Kussema <dev.kussema@gmail.com>
2024-03-11 11:01:05 +0100

    exemplo dotfile

---
+++ routes/nem_conf.ex
@@ -0,0 +1,31 @@
+{
+    "APP_ENV": "local",
+    "APP_DEBUG": true,
+    "MANUTENCAO_LEVE": true,
+    "allowed_hosts": {
+        "host3": "127.0.0.1"
+    },
+    "nested": {
+        "nested_key1": "nested_value1",
+        "nested_key2": "nested_value2"
+    },
+    "system": {
+        "db_conn": "mysql",
+        "db_host": "127.0.0.1",
+        "db_user": "root",
+        "db_passwd": "",
+        "db_port": 3306,
+        "db_database": "pharmacus"
+    },
+    "mail":{
+        "mailer":"smtp",
+        "host":"sandbox.smtp.mailtrap.io",
+        "port":"2525",
+        "username":"29d42037cf2b57",
+        "password":"8d7a53a60ac434",
+        "encryption":"tls",
+        "from_address":"hello@example.com",
+        "from_name":"A Equipa de suporte"
+    },
+    "PC_HOST": true
+}
