7a97f6d07fab5ef9d090f9124461e6d72a605748
Augusto Kussema <dev.kussema@gmail.com>
2024-03-25 18:50:24 +0100

    Continuar amanhã

--- resources/views/default/partials/notifies.blade.php
+++ resources/views/default/partials/notifies.blade.php
@@ -1,19 +1,23 @@
 <div class="row">
-    <div class="col-md-6 col-lg-6">
 
-        <div class="card mb-2">
-          <div class="row no-gutters">
-             <div class="col-md-6 col-lg-4">
-                <img src="{{ pharma('assets/images/page-img/08.jpg')}}" class="card-img" alt="#">
-             </div>
-             <div class="col-md-6 col-lg-8">
-                <div class="card-body">
-                   <h4 class="card-title" id="titulo">Card title</h4>
-                   <p class="card-text" id="descricao">This is a wider card with supporting text below as a natural lead-in. a little bit longer.</p>
-                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <div class="div_notificacao">
+        <div class="col-md-6 col-lg-6" style="display: none">
+            <div class="card mb-2">
+                <div class="row no-gutters">
+                    <div class="col-md-6 col-lg-4">
+                        <img src="{{ pharma('assets/images/page-img/08.jpg') }}" class="card-img" alt="#">
+                    </div>
+                    <div class="col-md-6 col-lg-8">
+                        <div class="card-body">
+                            <h4 class="card-title" id="titulo">Card title</h4>
+                            <p class="card-text" id="descricao">This is a wider card with supporting text below as a
+                                natural lead-in. a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
                 </div>
-             </div>
-          </div>
-       </div>
+            </div>
+        </div>
     </div>
+
 </div>
