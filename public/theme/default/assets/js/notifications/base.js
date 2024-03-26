936dffa386c77569d631004f0074c87bc0c717a9
Augusto Kussema <dev.kussema@gmail.com>
2024-03-26 10:06:36 +0100

    Já confirma, o resto continuamos depois.

--- public/theme/default/assets/js/notifications/base.js
+++ public/theme/default/assets/js/notifications/base.js
@@ -60,8 +60,69 @@ $(document).ready(function () {
                         });
                     }
                 } else {
-                    console.log('Erro ao obter notificação: ' + xhr.statusText);
+                    //console.log('Erro ao obter notificação: ' + xhr.statusText);
                 }
+            };
+            xhr.send();
+        }
+    }
+
+	var idNotifier = [];
+	function getNotificacaoGerente() {
+        // Verificar se a meta "is_ah" existe
+        if (document.querySelector('meta[name="is_ah_gerente"]')) {
+            // Obter o conteúdo da meta
+            var isAhContent = document.querySelector('meta[name="is_ah_gerente"]').getAttribute('content');
+    
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/getter/notificacao/' + isAhContent, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = function () {
+                if (response && response.length > 0) {
+					response.forEach(function (notification) {
+						if (!idNotifier.includes(notification.chave)) {
+							idNotifier.push(notification.chave); // Adiciona o valor no array
+							var segundo = 0;
+							if (idNotifier.length > 2) { // Se o array tiver mais de 2 valores
+								idNotifier.shift(); // Remove o primeiro (o mais antigo)
+								segundo = 1;
+							}
+
+							var notificationHtml = `
+								<div class="col-md-6 col-lg-6">
+									<div class="card mb-2">
+										<div class="row no-gutters">
+											<div class="col-md-6 col-lg-4">
+												<img src="/theme/default/assets/images/page-img/08.jpg" class="card-img" alt="#">
+											</div>
+											<div class="col-md-6 col-lg-8">
+												<div class="card-body">
+													<h4 class="card-title" id="titulo">${notification.titulo}</h4>
+													<p class="card-text" id="descricao">${notification.message}</p>
+													<p class="card-text"><small class="text-muted">Cerca de ${tempoDecorrido(notification.created_at)}</small>
+													<a href="/estoque/confirmar/${notification.chave}/${isAhContent}" class="btn btn-outline-primary btn-sm ml-2">Confirmar</a>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							`;
+
+							// Adiciona o HTML à div de notificação
+							$('#notification_content').append(notificationHtml);
+
+							// Exibe a div de notificação
+							$('#notification_content').removeClass('d-none');
+
+							// Faça algo se a resposta for diferente de 0, null ou false
+							toastr.info(notification.message, notification.titulo, {
+								closeButton: true,
+								progressBar: false
+							});
+						}
+					});
+				}
             };
             xhr.send();
         }
@@ -97,11 +158,6 @@ $(document).ready(function () {
 		return strTempoDecorrido;
 	}
 
-	// Exemplo de uso
-	var dataStr = "2024-03-26 08:18:38";
-	var tempoDecorridoStr = tempoDecorrido(dataStr);
-	console.log(tempoDecorridoStr); // Exibe "3 min atrás"
-
-
 	setInterval(getNotificacao, 2000);
+	setInterval(getNotificacaoGerente, 2000);
 });
