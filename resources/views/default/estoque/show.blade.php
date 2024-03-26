c78704a93ebb5559cbe898dd6b692de8cfc794b5
Augusto Kussema <dev.kussema@gmail.com>
2024-03-26 18:54:36 +0100

    ****

--- resources/views/default/estoque/show.blade.php
+++ resources/views/default/estoque/show.blade.php
@@ -110,42 +110,44 @@
                         <tbody class="ligth-body">
                             {{-- Gerado automaticamente --}}
                             @foreach ($estoque as $est)
-                                <tr>
-                                    <td>
-                                        <div class="checkbox d-inline-block">
-                                            <input type="checkbox" class="checkbox-input" id="checkbox2">
-                                            <label for="checkbox2" class="mb-0"></label>
-                                        </div>
-                                    </td>
-                                    <td><b>{{ $est->produto->designacao }}</b></td>
-                                    <td>{{ $est->produto->dosagem }}</td>
-                                    <td>{{ $est->produto->forma }}</td>
-                                    <td>{{ $est->produto->origem_destino }}</td>
-                                    <td>{{ $est->produto->num_lote }}</td>
-                                    <td><b>{{ $est->produto->grupo_farmaco->nome }}</b></td>
-                                    <td>{{ getCaixa($est->produto->descritivo) }}</td>
-                                    <td>{{ $est->produto->saldo->qtd }}</td>
-                                    <td>{{ $est->produto->num_documento }}</td>
-                                    <td>{{ $est->produto->data_expiracao }}</td>
-                                    <td>
-                                        <div class="d-flex align-items-center list-action">
-                                            @if (vPerm('produtos', ['dar_baixa']) or Auth::user()->isFarmacia)
-                                                <a class="badge bg-success mr-2" title="Dar baixa" href="javascript:void(0)"
-                                                    onclick="modalDarBaixa({{ $est->produto->id }}, '{{ getCaixa($est->produto->descritivo) }}')">
-                                                    <i class="ri-install-line mr-0"></i>
-                                                </a>
-                                           @endif
-                                            <a class="badge bg-info mr-2" data-toggle="tooltip" data-placement="top"
-                                                title="Editar" href="javascript:void(0)" onclick="modalEditarProdutoEstoque({{ $est->produto->id }})">
-                                                <i class="ri-pencil-line mr-0"></i>
-                                            </a>
-                                            <a class="badge bg-warning mr-2" data-toggle="tooltip" data-placement="top"
-                                                title="Eliminar" href="javascript:void(0)" onclick="">
-                                                <i class="ri-delete-bin-line mr-0"></i>
-                                            </a>
-                                        </div>
-                                    </td>
-                                </tr>
+                                @if ($est->produto->confirmado != 0)
+                                    <tr>
+                                        <td>
+                                            <div class="checkbox d-inline-block">
+                                                <input type="checkbox" class="checkbox-input" id="checkbox2">
+                                                <label for="checkbox2" class="mb-0"></label>
+                                            </div>
+                                        </td>
+                                        <td><b>{{ $est->produto->designacao }}</b></td>
+                                        <td>{{ $est->produto->dosagem }}</td>
+                                        <td>{{ $est->produto->forma }}</td>
+                                        <td>{{ $est->produto->origem_destino }}</td>
+                                        <td>{{ $est->produto->num_lote }}</td>
+                                        <td><b>{{ $est->produto->grupo_farmaco->nome }}</b></td>
+                                        <td>{{ getCaixa($est->produto->descritivo) }}</td>
+                                        <td>{{ $est->produto->saldo->qtd }}</td>
+                                        <td>{{ $est->produto->num_documento }}</td>
+                                        <td>{{ $est->produto->data_expiracao }}</td>
+                                        <td>
+                                            <div class="d-flex align-items-center list-action">
+                                                @if (vPerm('produtos', ['dar_baixa']) or Auth::user()->isFarmacia)
+                                                    <a class="badge bg-success mr-2" title="Dar baixa" href="javascript:void(0)"
+                                                        onclick="modalDarBaixa({{ $est->produto->id }}, '{{ getCaixa($est->produto->descritivo) }}')">
+                                                        <i class="ri-install-line mr-0"></i>
+                                                    </a>
+                                            @endif
+                                                {{-- <a class="badge bg-info mr-2" data-toggle="tooltip" data-placement="top"
+                                                    title="Editar" href="javascript:void(0)" onclick="modalEditarProdutoEstoque({{ $est->produto->id }})">
+                                                    <i class="ri-pencil-line mr-0"></i>
+                                                </a> --}}
+                                                {{-- <a class="badge bg-warning mr-2" data-toggle="tooltip" data-placement="top"
+                                                    title="Eliminar" href="javascript:void(0)" onclick="">
+                                                    <i class="ri-delete-bin-line mr-0"></i>
+                                                </a> --}}
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endif
                             @endforeach
                         </tbody>
                     </table>
