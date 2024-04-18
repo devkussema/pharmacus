6d48cba49b422fcf63f61582663aac51e868f2bd
Augusto Kussema <dev.kussema@gmail.com>
2024-04-18 09:30:23 +0100

    Ajustado

--- resources/views/default/doc_generate/novoDoc.blade.php
+++ resources/views/default/doc_generate/novoDoc.blade.php
@@ -8,14 +8,22 @@
     <style>
         body {
             font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
         }
 
         .header {
             text-align: center;
+            margin-bottom: 20px;
         }
 
         .tbl-container {
-            margin-top: 20px;
             margin-left: auto;
             margin-right: auto;
         }
@@ -29,6 +37,7 @@
         td {
             padding: 8px;
             text-align: center;
+            /* Centraliza o texto */
             border-bottom: 1px solid #ddd;
             white-space: nowrap;
             overflow: hidden;
@@ -39,34 +48,29 @@
             background-color: #f2f2f2;
         }
 
-        .signatures {
-      position: absolute;
-      bottom: 20px;
-      left: 0;
-      right: 0;
-      text-align: center;
-    }
+        .footer {
+            margin-top: auto;
+            text-align: center;
+            padding: 20px 0;
+        }
 
         .signature {
-            width: 20%;
-            /* Reduzir um pouco para acomodar espaço extra entre eles */
             display: inline-block;
             vertical-align: top;
-            margin: 0 50px;
+            margin: 0 10px;
+            /* Adiciona margens para separação */
+            text-align: center;
+            /* Centraliza o texto */
         }
 
         .signature-left {
             float: left;
             /* Alinhar à esquerda */
-            text-align: center;
-            /* Alinhar texto à esquerda */
         }
 
         .signature-right {
             float: right;
             /* Alinhar à direita */
-            text-align: center;
-            /* Alinhar texto à direita */
         }
 
         .clear {
@@ -77,62 +81,57 @@
 </head>
 
 <body>
+    <div class="container">
 
-    <div class="header">
-        <img src="{{ asset('assets/images/insignia.png') }}" width="50" height="50" alt="República de Angola">
-        <h4>República de Angola</h4>
-        <h4>Hospital Geral Especializado de Luanda</h4>
-        <h4>Ficha de Levantamento e Gasto de Medicamentos</h4>
-        <h4>Secção .....................................................................................</h4>
-        <h4>Data: __ /__/20__</h4>
-    </div>
-
-    <div class="tbl-container">
-        <table>
-            <thead>
-                <tr>
-                    <th>Nº</th>
-                    <th>Medicamento e Material Gastável</th>
-                    <th>Gastos</th>
-                    <th>Existência</th>
-                    <th>Quantidade Pedida</th>
-                    <th>Quantidade Disponibilizada</th>
-                    <th>Data de Expiração</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>3</td>
-                    <td>Paracetamol</td>
-                    <td>13</td>
-                    <td>Nenhuma</td>
-                    <td>4</td>
-                    <td>2</td>
-                    <td>4/01/2024</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>Ampicilina</td>
-                    <td>7</td>
-                    <td>Nenhuma</td>
-                    <td>2</td>
-                    <td>8</td>
-                    <td>6/01/2024</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-    <div class="signatures">
-        <div class="signature signature-left">
-            <p><b>O Diretor</b></p>
-            <p>_____________________</p>
+        <div class="header">
+            <img src="{{ asset('assets/images/insignia.png') }}" width="50" height="50" alt="República de Angola">
+            <h4>REPÚBLICA DE ANGOLA</h4>
+            <h4>HOSPITAL GERAL ESPECIALIZADO DE LUANDA</h4>
+            <h4>FICHA DE LEVANTAMENTO E GASTO DE MEDICAMENTOS</h4>
+            <h4>SECÇÃO .....................................................................................</h4>
+            <h4>DATA: __ / __/ 20__</h4>
         </div>
-        <div class="signature signature-right">
-            <p><b>O Diretor Gabinete</b></p>
-            <p>_____________________</p>
+
+        <div class="tbl-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nº</th>
+                        <th>Medicamento e Material Gastável</th>
+                        <th>Gastos</th>
+                        <th>Existência</th>
+                        <th>Quantidade Pedida</th>
+                        <th>Quantidade Disponibilizada</th>
+                        <th>Data de Expiração</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for ($i = 1; $i <= 10; $i++)
+                        <tr>
+                            <td>{{ $i }}</td>
+                            <td>Paracetamol</td>
+                            <td>{{ $i + 2 }}</td>
+                            <td>Nenhuma</td>
+                            <td>{{ $i + 3 }}</td>
+                            <td>2</td>
+                            <td>{{ $i }}/01/2024</td>
+                        </tr>
+                    @endfor
+                </tbody>
+            </table>
         </div>
-        <div class="clear"></div>
+
+        <div class="footer">
+            <div class="signature signature-left">
+                <p>CHEFE DE SECÇÃO</p>
+                <p>_____________________</p>
+            </div>
+            <div class="signature signature-right">
+                <p>DESPACHADO POR</p>
+                <p>_____________________</p>
+            </div>
+            <div class="clear"></div>
+        </div>
     </div>
 </body>
 
