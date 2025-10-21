5207f44066157c44d279ed063e2b7e65ecb0a01e
Augusto Kussema <dev.kussema@gmail.com>
2025-10-21 13:36:07 +0100

    feat: Add message notification icon to user menu for improved user interaction

--- app/Views/partials/header.blade.php
+++ app/Views/partials/header.blade.php
@@ -15,6 +15,10 @@
         </form>
     </div> --}}
     <ul class="nav user-menu float-end">
+        <li class="nav-item dropdown d-none d-md-block">
+                    <a href="javascript:void(0);" id="open_msg_box" class="hasnotifications nav-link"><img
+                            src="{{ assetr('assets/img/icons/note-icon-01.svg')}}" alt><span class="pulse"></span> </a>
+                </li>
         <li class="nav-item dropdown has-arrow user-profile-list">
             <a href="#" class="dropdown-toggle nav-link user-link" data-bs-toggle="dropdown">
                 <div class="user-names">
