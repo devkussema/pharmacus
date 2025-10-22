6225b442fa81ed6519cad32b83279ce97f62c8c0
Augusto Kussema <dev.kussema@gmail.com>
2025-10-22 18:08:45 +0100

    feat: Adicionar componente offcanvas para exibição de histórico de produtos no estoque

---
+++ resources/views/prepharma/estoque/_productHistory.blade.php
@@ -0,0 +1,20 @@
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+    <div class="offcanvas-header">
+        <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+        <button type="button" class="btn-close text-reset"
+            data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <div>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text,
+            images, lists, etc.
+        </div>
+        <h5 class="mt-3">List</h5>
+        <ul class="ps-3">
+            <li class>Nemo enim ipsam voluptatem quia aspernatur</li>
+            <li class>Neque porro quisquam est, qui dolorem</li>
+            <li class>Quis autem vel eum iure qui in ea</li>
+        </ul>
+    </div>
+</div>
