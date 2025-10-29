a22782146b279a7c2f7c97e3231245e54f164ca0
Augusto Kussema <dev.kussema@gmail.com>
2025-10-29 08:08:25 +0100

    feat: adicionar verificação para coluna telefone na tabela users antes da migração

--- database/migrations/2025_09_27_212128_add_telefone_to_users_table.php
+++ database/migrations/2025_09_27_212128_add_telefone_to_users_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        if (!Schema::hasColumn('users', 'telefone')) {
+            Schema::table('users', function (Blueprint $table) {
             $table->string('telefone')->nullable()->after('email');
-        });
+            });
+        }
     }
 
     /**
