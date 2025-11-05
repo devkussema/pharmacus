9917a6b81bd609266cfcd005524d464ba54d676c
Augusto Kussema <dev.kussema@gmail.com>
2025-11-05 19:31:06 +0100

    refactor: remover DiretorController e atualizar rotas para usar HomeController

--- Modules/Diretor/routes/web.php
+++ Modules/Diretor/routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Diretor\Http\Controllers\DiretorController;
+use Modules\Diretor\Http\Controllers\HomeController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('diretors', DiretorController::class)->names('diretor');
+Route::prefix('')->group(function () {
+    Route::resource('diretor', HomeController::class)->names('diretor');
 });
